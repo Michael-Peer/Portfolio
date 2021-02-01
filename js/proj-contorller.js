dbea3b8a72125745cbc5231952d787f5a4c260a4
Michael Peer <michaelpeer2004@gmail.com>
2021-02-01 23:09:39 +0200

    improve rendering

--- js/proj-contorller.js
+++ js/proj-contorller.js
@@ -1,4 +1,4 @@
-
+'use strict'
 
 //document.ready() IIFE
 $(function onInit() {
@@ -7,24 +7,6 @@ $(function onInit() {
   $('.contact-form').on('submit', onFormSubmit)
   renderPortfolio()
 })
-
-
-function onFormSubmit(ev) {
-  ev.preventDefault()
-
-  const inputs = $('.contact-form').serializeArray()
-  var email = $('#email-input').val()
-  var subject = $('#subject-input').val()
-  var msg = $('#message-input').val()
-  console.log(email, subject, msg)
-  const url = `https://mail.google.com/mail/?view=cm&fs=1&to=michaelpeer2004@gmail.com&su=${subject}&b
-    ody=${msg}`
-  window.open(url)
-  openCanvas()//close
-
-}
-
-
 
 function renderPortfolio() {
 
@@ -55,21 +37,60 @@ function renderPortfolio() {
 }
 
 function onOpenModal(projId) {
-  console.log(projId, "projif")
-  var proj = getProjById(projId)
 
+  var proj = getProjById(projId)
   const categories = proj.labels.join(", ")
+
+  var strHtml = `<h2>${proj.name}</h2>
+  <p class="item-intro text-muted">${proj.title}</p>
+  <img class="img-fluid d-block mx-auto modal-img" src="img/portfolio/${proj.id}.jpg" alt="">
+  <p class="modal-desc">${proj.desc}</p>
+  <ul class="list-inline">
+    <li>Date: January 2017</li>
+    <li class="modal-categories">Categories: ${categories}}</li>
+  </ul>
+  <button class="btn btn-primary" data-dismiss="modal" type="button">
+    <i class="fa fa-times"></i>
+    Close Project</button>
+    <button onclick="onGithunLinkClicked('${proj.url}')" class="btn btn-info btn-github" data-dismiss="modal" type="button">
+      <i class="fa fa-github mr-1"></i>
+      Go to Project</button>`
+
+  $('.modal-body').html(strHtml)
+
+
   // $('.portfolio-modal').show()
   // $('.portfolio-modal').attr("aria-hidden", "false")
-  $('.modal-body h2').text(proj.name)
-  $('.item-intro').text(proj.title)
-  $('.modal-desc').text(proj.desc)
-  $('.modal-img').attr('src', `img/portfolio/${proj.id}.jpg`)
-  $('.modal-categories').text(`Categories: ${categories}`)
-  $('.btn-github').data("url", proj.url)
+  // $('.modal-body h2').text(proj.name)
+  // $('.item-intro').text(proj.title)
+  // $('.modal-desc').text(proj.desc)
+  // $('.modal-img').attr('src', `img/portfolio/${proj.id}.jpg`)
+  // $('.modal-categories').text(`Categories: ${categories}`)
+  // $('.btn-github').data("url", proj.url)
 
-  $('.btn-github').on('click', function onGithunLinkClicked() {
-    var url = $(this).data("url")
-    window.open(url)
-  })
+  // $('.btn-github').on('click', function onGithunLinkClicked() {
+  // var url = $(this).data("url")
+  // window.open(url)
+  // })
+}
+
+
+function onGithunLinkClicked(url) {
+  window.open(url)
+}
+
+function onFormSubmit(ev) {
+  ev.preventDefault()
+
+  const inputs = $('.contact-form').serializeArray()
+  var email = $('#email-input').val()
+  var subject = $('#subject-input').val()
+  var msg = $('#message-input').val()
+  //add line breaks
+  var lbrMsg = msg.replace(/\r\n|\r|\n/g, "%0D%0A");
+
+  const url = `https://mail.google.com/mail/?view=cm&fs=1&to=michaelpeer2004@gmail.com&su=${subject}&body=${lbrMsg}`
+  window.open(url)
+  openCanvas() //close
+
 }
